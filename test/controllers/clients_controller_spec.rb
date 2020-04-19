a23bd22a327709b4b6654e6ba6bb415b16b55454
Hugo Lopez <hugo.lopez@bigcommerce.com>
2020-04-19 09:32:07 -0500

    FactoryBot addded

--- test/controllers/clients_controller_spec.rb
+++ test/controllers/clients_controller_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Root::Controllers::ClientsController, type: :controller do
+RSpec.describe ClientsController, type: :controller do
     describe "index" do
         it "display all the registered clients" do
             hugo = FactoryBot.create(:clients, first_name: 'Hugo', last_name: 'Mandujano')
